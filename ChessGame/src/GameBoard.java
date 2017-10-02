6d76b8658a509a4de0663311f0254ffaf3f781c6
PuChen7 <puchen8295@gmail.com>
2017-10-02 14:53:38 -0400

    initial set up

---
+++ ChessGame/src/GameBoard.java
@@ -0,0 +1,8 @@
+/* This class implements the game board */
+
+import java.util.ArrayList;
+
+public class GameBoard {
+    // the board should be 8x8 size
+    Piece[][] gameBoard = new Piece[8][8];
+}
