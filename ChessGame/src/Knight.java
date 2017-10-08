b7c22a5f73ba7ddfab46349b6252846017460759
PuChen7 <puchen8295@gmail.com>
2017-10-08 16:38:28 -0400

    finished King and Knight

--- ChessGame/src/Knight.java
+++ ChessGame/src/Knight.java
@@ -1,4 +1,17 @@
-/* This class has properties of knight */
+/*
+ * This class has properties of Knight
+ */
 
 public class Knight extends Piece{
+	
+	public Knight(){
+		role = "N";
+	}
+	
+	@Override
+	public boolean validMove(int rank2, int file2) {
+		int diff1 = Math.abs(rank2 - rank);
+		int diff2 = Math.abs(file2 - file);
+		return (diff1 == 2 && diff2 == 1) || (diff1 == 1 && diff2 == 2);
+	}
 }
